d39d1043a3e05dc6aafec83c685c90b748d16d74
San Dinh Trong <dinhsan200@gmail.com>
2026-04-09 14:34:00 +0700

    fix nav icon mobile

--- src/components/layout/vertical/NavToggle.tsx
+++ src/components/layout/vertical/NavToggle.tsx
@@ -2,6 +2,7 @@
 
 // Hook Imports
 import useVerticalNav from '@menu/hooks/useVerticalNav'
+import { Menu } from 'lucide-react'
 
 const NavToggle = () => {
   // Hooks
@@ -15,7 +16,26 @@ const NavToggle = () => {
     <>
       {/* <i className='tabler-menu-2 cursor-pointer' onClick={handleClick} /> */}
       {/* Comment following code and uncomment above code in order to toggle menu on desktop screens as well */}
-      {isBreakpointReached && <i className='tabler-menu-2 cursor-pointer' onClick={handleClick} />}
+      {isBreakpointReached && (
+        <button
+          onClick={handleClick}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: 40,
+            height: 40,
+            padding: 0,
+            border: 'none',
+            background: 'transparent',
+            cursor: 'pointer',
+            borderRadius: 8,
+            flexShrink: 0
+          }}
+        >
+          <Menu size={28} />
+        </button>
+      )}
     </>
   )
 }
